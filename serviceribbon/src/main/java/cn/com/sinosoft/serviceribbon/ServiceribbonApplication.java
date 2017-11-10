43679f7fdfb49ef13ec8e63b531749b1209bd578
luanwenjie <luanwenjie@sinosoft.com.cn>
2017-11-10 14:31:44 +0800

    123

---
+++ serviceribbon/src/main/java/cn/com/sinosoft/serviceribbon/ServiceribbonApplication.java
@@ -0,0 +1,12 @@
+package cn.com.sinosoft.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceribbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceribbonApplication.class, args);
+	}
+}
