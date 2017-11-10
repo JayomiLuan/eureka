8d56b908a2d78d68fbd45da011d59c296fb0a667
luanwenjie <luanwenjie@sinosoft.com.cn>
2017-11-10 14:34:31 +0800

    123

--- serviceribbon/src/main/java/cn/com/sinosoft/serviceribbon/ServiceribbonApplication.java
+++ serviceribbon/src/main/java/cn/com/sinosoft/serviceribbon/ServiceribbonApplication.java
@@ -2,11 +2,23 @@ package cn.com.sinosoft.serviceribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceribbonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceribbonApplication.class, args);
 	}
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+
 }
